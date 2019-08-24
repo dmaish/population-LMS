d0a6495773dde6c8ea650f3f04dbd8199ba5f7ca
dmaish <mainadaniel81@gmail.com>
2019-08-24 14:02:45 +0300

    can update

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -47,11 +47,30 @@ export default class LocationsController{
             const currentLocationName = req.body.name;
             const paramsKey = Object.keys(req.body);
             const key = paramsKey[1];
-            console.log('key',  key)
             const editableValue = req.body;
             const value = editableValue[key]
             const editParam = { $set: { [key]: value } }
-            console.log('dididid', editParam);
+            if (key === "maleResidents" || "femaleResident"){
+                const currentLocation = await Location.findOne({name: currentLocationName});
+                if (key === "maleResidents"){
+                    const femaleResidents = currentLocation.femaleResidents;
+                    const total = femaleResidents + value;
+                    const updatedLocation = await Location.updateOne({ name: currentLocationName }, { $set: { [key]: value, totalPopulation: total } } );
+                        return res.status(200).json({
+                            message: 'Location updated successfully',
+                            Location: updatedLocation,
+                            });
+                } else if(key === "femaleResidents") {
+                    const maleResidents = currentLocation.maleResidents;
+                    const total = maleResidents + value;
+                    const updatedLocation = await Location.updateOne({ name: currentLocationName }, { $set: { [key]: value, totalPopulation: total } } );
+                        return res.status(200).json({
+                            message: 'Location updated successfully',
+                            Location: updatedLocation,
+                            });
+                }
+            }
+
             const updatedLocation = await Location.updateOne({ name: currentLocationName }, editParam );
             return res.status(200).json({
                 message: 'Location updated successfully',
